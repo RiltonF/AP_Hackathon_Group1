c7cfe777be4db14d9f3807c81700f266548156e1
Bob <bobvanelst@gmail.com>
2016-03-17 17:22:35 +0100

    Initial Setup, no Database

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  var reloadPort = 35729, files;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    develop: {
+      server: {
+        file: 'bin/www'
+      }
+    },
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      server: {
+        files: [
+          'bin/www',
+          'app.js',
+          'routes/*.js'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      js: {
+        files: ['app/scripts/controllers/*.js', 'app/scripts/*.js'],
+        options: {
+          livereload: reloadPort
+        }
+      },
+      css: {
+        files: [
+          'app/styles/*.css'
+        ],
+        options: {
+          livereload: reloadPort
+        }
+      },
+      views: {
+        files: ['app/views/*.html', 'app/*.html'],
+        options: {
+          livereload: reloadPort
+        }
+      }
+    }
+  });
+
+  grunt.config.requires('watch.server.files');
+  files = grunt.config('watch.server.files');
+  files = grunt.file.expand(files);
+
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+    var done = this.async();
+    setTimeout(function () {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
+          var reloaded = !err && res.statusCode === 200;
+          if (reloaded) {
+            grunt.log.ok('Delayed live reload successful.');
+          } else {
+            grunt.log.error('Unable to make a delayed live reload.');
+          }
+          done(reloaded);
+        });
+    }, 500);
+  });
+
+  grunt.registerTask('default', [
+    'develop',
+    'watch'
+  ]);
+  //Go over this - Bob
+  /*grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'postcss:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('default', [
+    'develop',
+    'watch'
+  ]);*/
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# server
+
+To start server do command: 'node bin/www' , 'grunt', 'nodemon bin/www' from the root directory
+    --> Listening on localhost port 3000
+
+  workflow is as follows
+
+  express server --> sets up link to front end (angular/bootstrap)
+        |--> linked to view of angular ('./app') through routes(./routes)
+                |-->'scripts' contain constructors and basic setup, init is in ./app/scripts/app.js
+                |-->view holds the page templates
+
+   The first initialization of the home page might be a little slow.
